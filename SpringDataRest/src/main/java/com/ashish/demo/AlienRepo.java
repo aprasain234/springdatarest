99a7486e4c662c8638bc03601fbf9f900b1bb5d5
Ashish Prasain <40052138+aprasain234@users.noreply.github.com>
2020-03-11 15:15:42 -0500

    This is Spring Data REST

---
+++ SpringDataRest/src/main/java/com/ashish/demo/AlienRepo.java
@@ -0,0 +1,12 @@
+package com.ashish.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ashish.demo.model.Alien;
+
+@RepositoryRestResource(collectionResourceRel = "aliens", path="aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+	
+
+}
---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into alien values (1001, 'Ashish', 'Java');
+insert into alien values (1002, 'Prasain', 'Phython');
+insert into alien values (1003, 'Thor', 'Thunder');
+insert into alien values (1004, 'Captain America', 'Java');
+insert into alien values (1005, 'Tony', 'AI');
+insert into alien values (1006, 'Black Widow','Java');
+insert into alien values (1007, 'Clint', 'Arrow');
+insert into alien values (1008, 'Banner', '7PhDs');
